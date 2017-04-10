1480a8c6e175bfe41c636a19fffa413eebf8c166
Roxnya <chernroni@gmail.com>
2017-04-11 01:00:44 +0200

    Solution Commit

---
+++ CompareSolvers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareSolvers
+{
+    class Program
+    {
+        private void CompareSolvers()
+        {
+            //maze via maze dfs generator
+            //print maze
+            //solve via bfs
+            //print
+            //solve via dfs
+            //print
+            //print number of states for each solution  - generate a maze bif enough to feel the difference
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
